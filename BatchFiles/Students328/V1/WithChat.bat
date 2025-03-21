f519c8ac9d2f663b9a401c6426f32a68a5f8027f
keshavsoft <nknnkumar@yahoo.com>
2025-03-21 12:12:54 +0530

    stage 1

---
+++ BatchFiles/Students328/V1/WithChat.bat
@@ -0,0 +1,24 @@
+@echo off
+
+call npm run home
+xcopy .\FrontEnd\ForWebSocket\KeshavSoft .\public\Chat /h /i /c /k /e /r /y
+
+cd ..\CrudFrontEndGulp
+git pull
+
+call gulp build:publicDir
+
+xcopy .\publicDir ..\CrudBackEndWithMail\public\StudentsCRUD /h /i /c /k /e /r /y
+
+
+
+cd ..\FrontEndForClients
+git pull
+
+call npm run Students
+
+xcopy .\publicDir\Students ..\CrudBackEndWithMail\public\Students /h /i /c /k /e /r /y
+
+
+
+cd ..\CrudBackEndWithMail
