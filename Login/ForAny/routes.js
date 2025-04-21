7d2334ea7bf24079feb61dd2798a63fcf4a5d71f
Srivasu215 <sri.vasu215@gmail.com>
2025-04-21 12:26:37 +0530

    Login ForAny Check

--- Login/ForAny/routes.js
+++ Login/ForAny/routes.js
@@ -4,8 +4,10 @@ var router = express.Router();
 
 import { router as routerShowAll } from './ShowAll/routes.js';
 import { router as routerCreate } from './Create/routes.js';
+import { router as routerCheck } from './Check/routes.js';
 
 router.use('/ShowAll', routerShowAll);
 router.use('/Create', routerCreate);
+router.use('/Check', routerCheck);
 
 export { router };
